5f75f6a54adf459d68ca66ddd3f350d37ce541a5
Lais <moreira.lais@gmail.com>
2016-10-05 18:22:12 -0300

    ajustes testes

--- app/src/androidTest/java/br/com/lais/cinetop/DetailsTest.java
+++ app/src/androidTest/java/br/com/lais/cinetop/DetailsTest.java
@@ -5,40 +5,42 @@ import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import br.com.lais.cinetop.activity.*;
+import java.io.Serializable;
+
+import br.com.lais.cinetop.activity.DetailsActivity;
 import br.com.lais.cinetop.model.ResultJson;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
 @RunWith(AndroidJUnit4.class)
-public class PopMoviesTest {
+public class DetailsTest {
 
-    private Activity activity;
+
     private RecyclerView recyclerView;
+    private Activity launchActivity;
 
     @Rule
-    public ActivityTestRule testRule = new ActivityTestRule(PopMoviesActivity.class);
+    public ActivityTestRule testRule = new ActivityTestRule(DetailsActivity.class);
+
 
     @Before
     public void setUp()throws Exception
     {
-        activity = testRule.getActivity();
-        recyclerView = (RecyclerView) activity.findViewById(R.id.recycler_filme);
+        launchActivity = testRule.launchActivity(new Intent().putExtra("filme", new ResultJson("poster_path","original_title","vote_average",1)));
+        recyclerView = (RecyclerView) launchActivity.findViewById(R.id.recycler_details);
     }
 
     @Test
     public void activityNaoDeveSerNull()throws Exception
     {
-        assertNotNull(activity);
+        assertNotNull(launchActivity);
 
     }
 
@@ -50,9 +52,10 @@ public class PopMoviesTest {
     }
 
     @Test
-    public void cliqueRecyclerViewDeveIrNovaIntent() throws Exception{
-        //TODO
-        assertEquals(1,1);
+    public void deveTerIntent() throws Exception{
+        Serializable filme = launchActivity.getIntent().getSerializableExtra("filme");
+        assertNotNull(filme);
+
     }
 
 }
--- app/src/main/java/br/com/lais/cinetop/model/ResultJson.java
+++ app/src/main/java/br/com/lais/cinetop/model/ResultJson.java
@@ -12,6 +12,13 @@ public class ResultJson implements Serializable{
     String vote_average;// nota
     int id;
 
+    public ResultJson(String poster_path,String original_title,String vote_average,int id){
+        setPoster_path(poster_path);
+        setOriginal_title(original_title);
+        setVote_average(vote_average);
+        setId(id);
+    }
+
     public String getPoster_path() {
         return poster_path;
     }
@@ -26,5 +33,21 @@ public class ResultJson implements Serializable{
 
     public int getId() {
         return id;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public void setVote_average(String vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
